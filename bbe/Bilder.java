a4cfad70348756037d20e10c20fb75e8d85cc81b
Valentin Herrmann <accounts@valentin-herrmann.com>
2025-04-28 18:18:53 +0200

    Bilder Klasse

---
+++ bbe/Bilder.java
@@ -0,0 +1,12 @@
+package bbe;
+
+
+/**
+ * Alle verfügbaren Bilder (Entities und Hintergrund): <a href="https://gameboard.valentin-herrmann.com/resources/images.html">gameboard.valentin-herrmann.com/resources/images.html</a>
+ *
+ * @author Valentin Herrman
+ */
+public class Bilder
+{
+    // Diese Klasse ist nur ein Dummy, damit deie Bilder in den Docs auf oberster Ebene angezeigt werden.
+}
