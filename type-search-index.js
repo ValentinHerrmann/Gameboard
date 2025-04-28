170c4a5340fe93e5492d78314757788b15449058
ValentinHerrmann <accounts@valentin-herrmann.com>
2025-04-28 16:19:40 +0000

    Deploying to javadoc from @ ValentinHerrmann/Gameboard@a4cfad70348756037d20e10c20fb75e8d85cc81b 🚀

--- type-search-index.js
+++ type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"bbe","l":"ExampleEntity"},{"p":"bbe","l":"Gameboard"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"bbe","l":"Bilder"},{"p":"bbe","l":"ExampleEntity"},{"p":"bbe","l":"Gameboard"}];updateSearchResults();
